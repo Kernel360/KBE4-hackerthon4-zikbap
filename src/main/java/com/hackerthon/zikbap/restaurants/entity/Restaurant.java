ad42824566d602336dd9e319ecc4bfe55ca0c2d0
missiletoe <johnsonheo96@gmail.com>
2025-03-11 14:58:50 +0900

    added Restaurant.java

--- src/main/java/com/hackerthon/zikbap/restaurants/entity/Restaurant.java
+++ src/main/java/com/hackerthon/zikbap/restaurants/entity/Restaurant.java
@@ -1,33 +1,26 @@
 package com.hackerthon.zikbap.restaurants.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.math.BigDecimal;
-
 @Entity
-@Table(name = Restaurant.TABLE_NAME)
+@Table(name = "restaurants")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Restaurant {
 
-    public static final String TABLE_NAME = "restaurant";
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    private String name;
-    private String address;
+    private Long restaurantId;
+    private String restaurantName;
+    private String locationCategory;
+    private String locationDetail;
+    private float rating;
     private String phoneNumber;
     private String email;
-    private BigDecimal rating;
 }
