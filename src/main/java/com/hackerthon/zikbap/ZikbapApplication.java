a01cf111cbfd694bcde6c1e5fd1f7736ff30ce91
Dsys1129 <dsys1129@gmail.com>
2025-03-11 11:39:48 +0900

    Init Project

---
+++ src/main/java/com/hackerthon/zikbap/ZikbapApplication.java
@@ -0,0 +1,13 @@
+package com.hackerthon.zikbap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZikbapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZikbapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hackerthon/zikbap/ZikbapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackerthon.zikbap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZikbapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
