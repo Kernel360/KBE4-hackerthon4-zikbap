904b78ca72ebc4dae0e299068e27b9ac5386b320
채지원 <dev.julia.chae@gmail.com>
2025-03-12 11:58:34 +0900

    음식점 가격순 쿼리 수정

--- src/main/java/com/hackerthon/zikbap/restaurants/repository/RestaurantRepository.java
+++ src/main/java/com/hackerthon/zikbap/restaurants/repository/RestaurantRepository.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
-  @Query("select r from Restaurant r where r.locationCategory =:locationCategory")
+  @Query("select r from Restaurant r where r.locationCategory =:locationCategory order by r.rating DESC ")
   List<Restaurant> findAllByOrderByRatingDesc(String locationCategory);
 
   @Query("SELECT r from Restaurant r join Menu m on r.restaurantId = m.restaurant.restaurantId where m.signature = 1 and r.locationCategory =:locationCategory order by m.price asc")
